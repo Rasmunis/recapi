cc265734fdb25a202b4a4b9be5410fdc5d5b11de
rasmus.stene <rasmus.stene@cgi.com>
2020-10-15 14:14:07 +0200

    Add models for Recipe, Ingredient and join-table

---
+++ RecAPI/Models/RecipeIngredient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecAPI.Models
+{
+    public class RecipeIngredient
+    {
+        public int RecipeId { get; set; }
+        public Recipe Recipe { get; set; }
+        public int IngredientId { get; set; }
+        public Ingredient Ingredient { get; set; }
+        public decimal Amount { get; set; }
+        public Unit Unit { get; set; }
+    }
+
+    public enum Unit
+    {
+        Pcs,
+        Tablespoons,
+        Teaspoons,
+        Grams,
+        Deciliters,
+        Liters,
+        Cloves
+    }
+}
