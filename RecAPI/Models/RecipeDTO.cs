7c4781a4a873e5ba58c3a552ffa350b20f9f93a7
rasmus.stene <rasmus.stene@cgi.com>
2020-10-15 15:07:00 +0200

    Create DTOs for Recipe, Ingredient and RecipeIngredient

---
+++ RecAPI/Models/RecipeDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace RecAPI.Models
+{
+	public class RecipeDTO
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string Instructions { get; set; }
+		public List<RecipeIngredientDTO> RecipeIngredients { get; set; }
+	}
+}
---
+++ RecAPI/Models/RecipeIngredientDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecAPI.Models
+{
+    public class RecipeIngredientDTO
+    {
+        public int RecipeId { get; set; }
+        public string RecipeName { get; set; }
+        public int IngredientId { get; set; }
+        public string IngredientName { get; set; }
+        public decimal Amount { get; set; }
+        public Unit Unit { get; set; }
+    }
+}
